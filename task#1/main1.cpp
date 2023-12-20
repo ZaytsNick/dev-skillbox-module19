61c06521784b638bddb27c8512edba86a0234762
ZaytsNick <10kcin01@gmail.com>
2023-12-20 19:30:28 +0500

    task#1

--- task#1/main1.cpp
+++ task#1/main1.cpp
@@ -6,9 +6,8 @@ int main()
 {
     std::ifstream words;
     std::string word;
-<<<<<<< HEAD
     int answer(0);
-    words.open("wordss.txt");
+    words.open("words.txt");
     if (words.is_open())
     {
         std::cout << "Enter the word you want to find: ";
@@ -25,18 +24,6 @@ int main()
     {
         std::cout << "ERROR: The file did not open!" << std::endl;
         return 0;
-=======
-    std::cout << "Enter the word you want to find: ";
-    std::cin >> word;
-    int answer(0);
-    words.open("words.txt");
-    while (!words.eof())
-    {
-        std::string temp_word;
-        words >> temp_word;
-        if (temp_word == word)
-            answer++;
->>>>>>> 18f2225deb56b1ca130d9887686132914c7ad27a
     }
     std::cout << "Answer: " << answer << std::endl;
 }
