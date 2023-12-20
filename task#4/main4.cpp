03df87d47cd1c6a11d02efb07b97306770396ab3
ZaytsNick <10kcin01@gmail.com>
2023-12-20 19:31:17 +0500

    task#4

--- task#4/main4.cpp
+++ task#4/main4.cpp
@@ -21,7 +21,6 @@ int main()
             std::cout << "This file has extension .png" << std::endl;
         else
             std::cout << "ERROR: This file doesn't have extension .png" << std::endl;
-<<<<<<< HEAD
         char first_four_byte[4];
         PNG_file.read(first_four_byte, sizeof(first_four_byte));
         if (first_four_byte[0] == -119 &&
@@ -37,35 +36,4 @@ int main()
         }
     }
 }
-=======
-        char first_byte;
-        PNG_file.read((char *)&first_byte, sizeof(first_byte));
-        if (first_byte != -119)
-        {
-            std::cout << "ERROR: Analysis showed that this file is not in PNG format" << std::endl;
-            return 0;
-        }
-        else
-        {
-            char second_byte;
-            std::string temp_string;
-            for (int i = 0; i < 4; i++)
-            {
-                PNG_file.seekg(i + 1);
-                PNG_file.read((char *)&second_byte, sizeof(second_byte));
-                if (second_byte != '\r')
-                temp_string += second_byte;
-            }
-            if (temp_string != "PNG")
-            {
-                std::cout << "ERROR: Analysis showed that this file is not in PNG format" << std::endl;
-                return 0;
-            }
-        }
-    }
-    else
-        std::cout << "ERROR: The file did not open!" << std::endl;
-    PNG_file.close();
-    std::cout << "File " << path << " is in PNG format" << std::endl;
-}
->>>>>>> 18f2225deb56b1ca130d9887686132914c7ad27a
+
